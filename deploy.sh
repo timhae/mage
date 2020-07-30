948ccc0e551737f592a6198c22775a4d5ab11f23
Tim Haering <tim.haering@gmail.com>
2020-07-30 15:50:04 +0200

    adapt server settings and connect dialog to include uberspace, add build scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p maven openjdk11 protobuf
+
+./clean_dbs.sh
+pushd .
+pushd Mage.Client
+mvn assembly:assembly
+pushd target
+scp mage-client.zip root@hetzner1:/var/www/haering.dev/
+popd
+popd
+pushd Mage.Server
+mvn assembly:assembly
+pushd target
+scp mage-server.zip autarch@bernardi.uberspace.de:/home/autarch/mage-server_no_backup/
+popd
+popd
