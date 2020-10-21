d4533b6c93cc76c76137909a5547469dc156b8a5
Tim Haering <tim.haering@gmail.com>
2020-10-21 21:55:04 +0200

    change client deploy location

--- Mage.Client/deploy.sh
+++ Mage.Client/deploy.sh
@@ -2,4 +2,4 @@
 #!nix-shell -i bash -p maven openjdk11 protobuf
 
 mvn assembly:assembly
-scp target/mage-client.zip root@hetzner1:/var/www/haering.dev/
+scp target/mage-client.zip tim@netcup1:/var/www/haering.dev/
